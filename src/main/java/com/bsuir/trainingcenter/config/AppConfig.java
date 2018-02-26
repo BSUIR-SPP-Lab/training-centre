51043a58348149bea74b188d76f9007c6653c381
Vlad Belykh <Uladzislau.belykh@gmail.com>
2018-02-26 14:33:22 +0300

    init project

---
+++ src/main/java/com/bsuir/trainingcenter/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.bsuir.trainingcenter.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasouce")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+
+}
