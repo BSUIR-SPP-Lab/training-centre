8fb3e37949fb755855513ff93d5e35367e7940f8
Vlad Belykh <Uladzislau.belykh@gmail.com>
2018-03-16 12:03:35 +0300

    Tests for all services

---
+++ src/test/java/com/bsuir/trainingcenter/service/impl/TaskInfoServiceImplTest.java
@@ -0,0 +1,71 @@
+package com.bsuir.trainingcenter.service.impl;
+
+import com.bsuir.trainingcenter.dao.TaskInfoDAO;
+import com.bsuir.trainingcenter.entity.TaskInfo;
+import com.bsuir.trainingcenter.service.TaskInfoService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class TaskInfoServiceImplTest {
+
+    @MockBean
+    public TaskInfoDAO taskInfoDAO;
+
+    @Autowired
+    public TaskInfoService taskInfoService;
+
+    List<TaskInfo> list;
+    TaskInfo taskInfo;
+
+    @Before
+    public void setUp() {
+        list = new ArrayList<>();
+        taskInfo = new TaskInfo(1, "test", "testNotes");
+        list.add(taskInfo);
+
+    }
+
+    @Test
+    public void addTaskInfo() {
+        given(taskInfoDAO.addTaskInfo(taskInfo)).willReturn(true);
+        assertTrue(taskInfoService.addTaskInfo(taskInfo));
+    }
+
+    @Test
+    public void findTasksInfo() {
+        given(taskInfoDAO.findTasksInfo()).willReturn(list);
+        assertEquals(taskInfoService.findTasksInfo(),list);
+    }
+
+    @Test
+    public void findTaskInfo() {
+        given(taskInfoDAO.findTaskInfo(1)).willReturn(taskInfo);
+        assertEquals(taskInfoService.findTaskInfo(1),taskInfo);
+    }
+
+    @Test
+    public void updateTaskInfo() {
+        given(taskInfoDAO.updateTaskInfo(taskInfo)).willReturn(true);
+        assertTrue(taskInfoService.updateTaskInfo(taskInfo));
+    }
+
+    @Test
+    public void deleteTaskInfo() {
+        given(taskInfoDAO.deleteTaskInfo(1)).willReturn(true);
+        assertTrue(taskInfoService.deleteTaskInfo(1));
+    }
+}
---
+++ src/test/java/com/bsuir/trainingcenter/service/impl/TaskServiceImplTest.java
@@ -0,0 +1,79 @@
+package com.bsuir.trainingcenter.service.impl;
+
+import com.bsuir.trainingcenter.dao.TaskDAO;
+import com.bsuir.trainingcenter.entity.Task;
+import com.bsuir.trainingcenter.entity.view.TaskView;
+import com.bsuir.trainingcenter.service.TaskService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class TaskServiceImplTest {
+
+    @MockBean
+    public TaskDAO taskDAO;
+
+    @Autowired
+    public TaskService taskService;
+
+    List<Task> list;
+    Task task;
+    List<TaskView> listResult;
+    TaskView taskView;
+
+    @Before
+    public void setUp() {
+        list = new ArrayList<>();
+        LocalDateTime l = LocalDateTime.now();
+        task = new Task(1, 1,1,1, l);
+        list.add(task);
+        listResult = new ArrayList<>();
+        taskView = new TaskView(1, 1,1,1,l.toString());
+        listResult.add(taskView);
+
+    }
+
+    @Test
+    public void addTask() {
+        given(taskDAO.addTask(task)).willReturn(true);
+        assertTrue(taskService.addTask(taskView));
+    }
+
+    @Test
+    public void findTasks() {
+        given(taskDAO.findTasks()).willReturn(list);
+        assertEquals(taskService.findTasks(),listResult);
+    }
+
+    @Test
+    public void findTask() {
+        given(taskDAO.findTask(1)).willReturn(task);
+        assertEquals(taskService.findTask(1),taskView);
+    }
+
+    @Test
+    public void updateTask() {
+        given(taskDAO.updateTask(task)).willReturn(true);
+        assertTrue(taskService.updateTask(taskView));
+    }
+
+    @Test
+    public void deleteTask() {
+        given(taskDAO.updateTask(task)).willReturn(true);
+        assertTrue(taskService.updateTask(taskView));
+    }
+}
