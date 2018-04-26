acda36a253e329febbb0d1802d7b0d72561353a9
Vasya <inqire@gmail.com>
2018-04-27 00:49:00 +0300

    fix bag with animation.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -12,6 +12,7 @@ export class AuthComponent implements OnInit, OnDestroy {
 
 
   @HostBinding('@fade') a = true;
+
   destroy = true;
   constructor(private router: Router) {}
   ngOnInit(): void {
